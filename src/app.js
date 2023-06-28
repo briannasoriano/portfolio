562f36dc95e57375a01a109688e69af6f49a0ca6
briannasoriano <soriano.brianna01@gmail.com>
2023-06-28 09:42:01 -0400

    Revert "Update 2"
    This reverts commit a9f33c9f979070def1ed3f3affdf98f363d6c39c.

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
-
-function toggleButton(){
-    navList.classList.toggle('show')  //makes class list named show//
-}
-//where is it getting "click" from//
-hamburgerButton.addEventListener('click', toggleButton)
